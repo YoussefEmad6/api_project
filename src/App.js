36896057375e2cc2b307a7ca5a627d2aeb4df226
youssef917 <o@gmail.com>
2023-02-07 18:50:40 +0200

    catch data from the api and make a coin icon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="App">
+      {coins.map((coin) => {
+        return <Coin key={coin.id} name={coin.name} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
